11550f99c707faca7a2a89f174756bfe8ff17003
HeoYurim <mh10v2@naver.com>
2022-07-15 17:11:08 +0900

    연결 설정 개선, 커넥션 풀로 성능개선, JNDI, 커넥션 풀 설정, context.xml 수정, 커넥션 풀 동작 검, 테스트 코드

--- MustHaveJSP/WebContent/05_JDBC/ConnectionTest.jsp
+++ MustHaveJSP/WebContent/05_JDBC/ConnectionTest.jsp
@@ -1,3 +1,4 @@
+<%@page import="common.DBConnPool"%>
 <%@page import="common.JDBConnect"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
@@ -12,6 +13,29 @@
 	<%
 	JDBConnect jdbc1 = new JDBConnect(); //연결
 	jdbc1.close(); //연결 해제
+	%>
+	
+	<h2>JDBC 테스트2</h2>
+	<%
+	String driver = application.getInitParameter("OracleDriver");
+	String url = application.getInitParameter("OracleURL");
+	String id = application.getInitParameter("OracleId");
+	String pwd = application.getInitParameter("OraclePwd");
+	
+	JDBConnect jdbc2 = new JDBConnect(driver, url, id, pwd);
+	jdbc2.close();
+	%>
+	
+	<h2>JDBC 테스트3</h2>
+	<%
+	JDBConnect jdbc3 = new JDBConnect(application);
+	jdbc3.close();
+	%>
+	
+	<h2>커넥스 풀 테스트 4</h2>
+	<%
+	DBConnPool pool = new DBConnPool();
+	pool.close();
 	%>
 </body>
 </html>
--- MustHaveJSP/src/common/JDBConnect.java
+++ MustHaveJSP/src/common/JDBConnect.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.servlet.ServletContext;
+
 public class JDBConnect {
 	public Connection con; //데이터베이스 연결을 담당
 	public Statement stmt; //인파라미터가 없는 정적쿼리문 실행
@@ -14,7 +16,8 @@ public class JDBConnect {
 	//?로 표현하는데 sql문법 대체용
 	public ResultSet rs; //select 쿼리문 결과를 저장할 때 사용
 	
-	public JDBConnect() {
+	//기본 생성자
+	public JDBConnect() { 
 		try {
 			//JDBC 드라이버 로드
 			Class.forName("oracle.jdbc.OracleDriver");
@@ -29,6 +32,43 @@ public class JDBConnect {
 			
 		}catch(Exception e) {
 			e.printStackTrace(); //예외 로그 출력
+		}
+	}
+	
+	//두 번째 생성자
+	public JDBConnect(String driver, String url, String id, String pwd) {
+		try {
+			//JDBC 드라이버 로드
+			Class.forName(driver);
+			
+			//DB에 연결
+			con = DriverManager.getConnection(url, id, pwd);
+			
+			System.out.println("DB 연결 성공 (인수 생성자1)");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//세 번째 생성자(ServletContext를 활용)
+	public JDBConnect(ServletContext application) {
+		try{
+			//JDBC 드라이버 로드
+			String driver = application.getInitParameter("OracleDriver");
+			Class.forName(driver);
+			
+			//DB에 연결
+			String url = application.getInitParameter("OracleURL");
+			String id = application.getInitParameter("OracleId");
+			String pwd = application.getInitParameter("OraclePwd");
+			con = DriverManager.getConnection(url, id, pwd);
+			
+			System.out.println("DB 연결 성공(인수 생성자 2)");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
 		}
 	}
 	
